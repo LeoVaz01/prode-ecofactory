e524b7fd2a1dc43803d139d302327d5a7a633f17
LeoVaz01 <lenpvazquez@gmail.com>
2026-06-09 23:43:53 -0300

    Delete supabaseClient.ts

--- supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-TypeScript
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://tqlcfmrtszgrtmnuydhl.supabase.co'
-const supabaseAnonKey = 'sb_publishable_5zzjcayXXzXzRxeWO6V08Q_-c5GE-cA'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
